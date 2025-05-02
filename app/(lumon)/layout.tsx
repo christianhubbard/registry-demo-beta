65c63b55ecc1371df8f44ce214ede160355b276f
Christian Hubbard <Chrishubbard92@hotmail.com>
2025-05-02 13:47:08 -0400

    Update layout.tsx

--- app/(lumon)/layout.tsx
+++ app/(lumon)/layout.tsx
@@ -2,10 +2,10 @@ import type React from "react";
 import "@/app/globals.css";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/toaster";
-import { Geist_Mono } from "next/font/google";
+
 import { AppSidebar } from "@/components/dashboard/app-sidebar";
 
-const geistMono = Geist_Mono({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Lumon application",
@@ -16,7 +16,7 @@ export const metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={geistMono.className}>
+      <body >
         <main className="flex justify-center w-full">
           <div className="container">{children}</div>
         </main>
